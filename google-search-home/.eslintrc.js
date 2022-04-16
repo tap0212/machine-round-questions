3cb3e321b9167e7ae91746e8efe4903511f6b4d4
tap0212 <tapan.awasthi02@gmail.com>
2022-04-16 20:42:06 +0530

    initialized  🚀

---
+++ google-search-home/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'global-require': 0,
+    'no-console': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        '': 'never',
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+};
